2f90330354c38401fbd13292728e92e8d945baf7
Faizan <faizansuhail786@gmail.com>
2023-09-11 16:59:16 +0500

    update the newsletter and send the data to mailchimp for subscription

--- client/src/components/Newsletter/Newsletter.jsx
+++ client/src/components/Newsletter/Newsletter.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import './Newsletter.scss'
+import React from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import "./Newsletter.scss";
 
 const Newsletter = () => {
-  return (
-    <div className='newsletter'>
-      <div className="newstitle">Subscribe for latest news</div>
-      <div className="newsdesc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque tempore laboriosam iste totam maiores quo!</div>
-      <div className="inputfield">
-        <input type="text" className='input' placeholder='Enter your email '/>
-        <button className='subscribe'>Subscribe</button>
-      </div>
-    </div>
-  )
-}
+	const url =
+		"https://gmail.us8.list-manage.com/subscribe/post?u=0c5b9807532cae00d7def18ce&id=b1d8bb849e";
 
-export default Newsletter
+	return (
+		<div className="newsletter">
+			<div className="newstitle">Subscribe for the latest news</div>
+			<div className="newsdesc">
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque
+				tempore laboriosam iste totam maiores quo!
+			</div>
+      <div className="inputfield">
+			<MailchimpSubscribe url={url}>
+					{({ subscribe, status, message }) => (
+						<div>
+							<input
+								type="email"
+								className="input"
+								placeholder="Enter your email"
+							/>
+							<button
+								className="subscribe"
+								onClick={() => subscribe()}
+							>
+								Subscribe
+							</button>
+							{status === "sending" && <p>Sending...</p>}
+							{status === "error" && <p>{message}</p>}
+							{status === "success" && (
+								<p>Thank you for subscribing!</p>
+							)}
+						</div>
+					)}
+			</MailchimpSubscribe>
+				</div>
+		</div>
+	);
+};
+
+export default Newsletter;
